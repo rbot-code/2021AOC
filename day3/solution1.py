c1de9ab3b355e13ff25b59d2847a0662504d1a57
Russ <di8it@pm.me>
2021-12-23 00:33:01 -0500

    day3pt1 workingish

--- day3/solution1.py
+++ day3/solution1.py
@@ -7,7 +7,7 @@ with open("input") as file:
         for i, dat in enumerate(data):
             total[i] = total[i] + 1
             if dat == "1":
-                numOnes[i] = numOnes[1] + 1
+                numOnes[i] = numOnes[i] + 1
 
 #Create gamma rate
 gamma = [0]*dataLen
@@ -17,5 +17,26 @@ for i, each in enumerate(total):
     if numOnes[i] > (each/2):
         gamma[i] = 1
 
+#Epsilon is just the opposite of gamma
+epsilon = [0]*dataLen
+for i, val in enumerate(gamma):
+    if val == 0:
+        epsilon[i] = 1
+
 print(gamma)
-#epsilon
+print(epsilon)
+#Not yet figured out how to convert to binary and ints from list
+
+#gamma = ''.join(gamma)
+#print(bytes(gamma))
+#print(int.from_bytes(bytes(gamma)))
+#print((int(bin(gamma))) * (int(bin(epsilon))))
+
+#Converted by hand:
+#gamma = 2663
+#epsilon = 1432
+
+#Helpful sites:
+#https://stackoverflow.com/questions/38935169/convert-elements-of-a-list-into-binary
+#https://wiki.python.org/moin/BitwiseOperators
+#https://docs.python.org/3/library/stdtypes.html
